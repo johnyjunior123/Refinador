de6877c11c9da00a1c35927108e1742a8298818b
Johny Junior <johnyjunior64@gmail.com>
2023-08-12 18:42:48 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,2 @@
-config = ["postgres", "demon123", "127.0.0.1", "noticias"]
-
+config = ["postgres", "demon123", "127.0.0.1", "noticias"] # alterar para os dados de entrada de seu sgbd
 enderecos = "../data/enderecos.xlsx"
